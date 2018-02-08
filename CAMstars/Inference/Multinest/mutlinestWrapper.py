dd21bd4ec9d06d73d3725a36fa59135eb5ff8a7b
Adam Jermyn <adamjermyn@gmail.com>
2018-02-08 13:15:24 +0000

    Updated multinest wrapper file.

--- CAMstars/Inference/Multinest/mutlinestWrapper.py
+++ CAMstars/Inference/Multinest/mutlinestWrapper.py
@@ -164,6 +164,72 @@ def plot1D(a, parameters, outputDirectory, outputPrefix):
 		plt.close()
 	pp.close()
 
+def plot2D(a, parameters, outputDirectory, outputPrefix):
+	'''
+	Helper function for plotting 2D distributions from PyMultiNest. 
+
+	The arguments are:
+
+		a 				-	A PyMultiNest Analyzer object.
+		parameters		-	A list containing the names of all parameters.
+		outputDirectory	-	The directory in which to place output files.
+		outputPrefix	-	The prefix for all output file names.
+
+	There is no return value, as all output is written to files.
+	'''
+
+	prefix = outputDirectory + '/' + outputPrefix
+
+	n_params = len(parameters)
+	p = pymultinest.PlotMarginal(a)
+	values = a.get_equal_weighted_posterior()
+	modes = s['modes']
+
+	plt.figure(figsize=(5*n_params, 5*n_params))
+	for i in range(n_params):
+		plt.subplot(n_params, n_params, i + 1)
+		plt.xlabel(parameters[i])
+	
+		m = s['marginals'][i]
+		plt.xlim(m['5sigma'])
+	
+		oldax = plt.gca()
+		x,w,patches = oldax.hist(values[:,i], bins=nbins, edgecolor='grey', color='grey', histtype='stepfilled', alpha=0.2)
+		oldax.set_ylim(0, x.max())
+	
+		newax = plt.gcf().add_axes(oldax.get_position(), sharex=oldax, frameon=False)
+		p.plot_marginal(i, ls='-', color='blue', linewidth=3)
+		newax.set_ylim(0, 1)
+	
+		ylim = newax.get_ylim()
+		y = ylim[0] + 0.05*(ylim[1] - ylim[0])
+		center = m['median']
+		low1, high1 = m['1sigma']
+		#print(center, low1, high1)
+		newax.errorbar(x=center, y=y,
+			xerr=numpy.transpose([[center - low1, high1 - center]]), 
+			color='blue', linewidth=2, marker='s')
+		oldax.set_yticks([])
+		#newax.set_yticks([])
+		newax.set_ylabel("Probability")
+		ylim = oldax.get_ylim()
+		newax.set_xlim(m['5sigma'])
+		oldax.set_xlim(m['5sigma'])
+		#plt.close()
+	
+		for j in range(i):
+			plt.subplot(n_params, n_params, n_params * (j + 1) + i + 1)
+			p.plot_conditional(i, j, bins=20, cmap = plt.cm.gray_r)
+			for m in modes:
+				plt.errorbar(x=m['mean'][i], y=m['mean'][j], xerr=m['sigma'][i], yerr=m['sigma'][j])
+			plt.xlabel(parameters[i])
+			plt.ylabel(parameters[j])
+			#plt.savefig('cond_%s_%s.pdf' % (params[i], params[j]), bbox_tight=True)
+			#plt.close()
+
+	plt.savefig(prefix + 'marg.pdf')
+	plt.savefig(prefix + 'marg.png')
+	plt.close()
 
 
 
