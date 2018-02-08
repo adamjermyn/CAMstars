d303aa530b2791b00f12aedea9fe60087090f27f
Adam Jermyn <adamjermyn@gmail.com>
2018-02-08 14:55:20 +0000

    Added abundance aggregation helper methods to population class.

--- CAMstars/Material/population.py
+++ CAMstars/Material/population.py
@@ -16,6 +16,24 @@ class population:
 		# Dictionary with species as keys and all associated materials as values.
 		self.speciesDict = {n:list(m for m in materials if n in m.names) for n in self.species}
 
+		# We want to calculate the mean and standard deviation for the population.
+		# These are stored in numpy arrays indexed in the same order as species.
+		# For the mean we weight measurements by the reciprocal of their variance.
+		# For the variance we use the relaibility-weighted unbiased estimator, given by
+		# sum_i (x_i - mu)^2 / dx_i^2 / (V_1 - V_2 / V_1),
+		# where mu is the estimated mean, V_1 = sum_i 1/dx_i^2, and V_2 = sum_i 1/dx_i^4.
+		# This is as per the GSL Manual: https://www.gnu.org/software/gsl/manual/html_node/Weighted-Samples.html
+		self.logX = np.zeros(len(self.species))
+		self.dlogX = np.zeros(len(self.species))
+		for i,s in enumerate(self.species):
+			mats = self.speciesDict[s]
+			lx, dlx = zip(*[m.query(s) for m in mats])
+			self.logX[i] = np.average(lx, weights=1/dlx**2)
+			self.dlogX[i] = np.average((lx - self.logX[i])**2, weights=1/dlx**2)
+			v1 = np.sum(1/dlx**2)
+			v2 = np.sum(1/dlx**4)
+			self.dlogX[i] /= (v1 - v2 / v1)
+
 	def __add__(self, other):
 		'''
 		Combines two populations.
@@ -29,3 +47,18 @@ class population:
 			print('NOTE: Overlapping materials detected.')
 
 		return population(materials)
+
+	def query(self, name):
+		'''
+		Returns a list of tuples, each of which contains the name of a
+		material containing the specified species along with the abundance and
+		its associated uncertainty.
+		'''
+
+		ret = []
+		for m in self.materials:
+			q = m.query(name)
+			if q is not None:
+				ret.append((m.name, q[0], q[1]))
+
+		return ret
