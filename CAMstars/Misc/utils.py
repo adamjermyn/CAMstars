6ca5517abb94ec98277f2adbcc5c5632f0d02136
Adam Jermyn <adamjermyn@gmail.com>
2018-02-08 17:24:15 +0000

    Added error propagation method.

--- CAMstars/Misc/utils.py
+++ CAMstars/Misc/utils.py
@@ -8,3 +8,33 @@ def gaussianLogLike(x):
 	# |y - y0|/(dy_left) for y < y0.
 	return pref - x**2
 
+def propagate_errors(func, x, dx, eps=1e-3):
+	'''
+	The arguments are:
+		func 	-	The function with which to propagate errors. May take a multidimensional
+					input.
+		x 		-	The point at which to propagate errors. May be multidimensional.
+		dx 		-	The standard deviation of x. May be multidimensional.
+		eps 	-	A small value to use in computing derivatives. Default is 1e-3.
+
+	The return value is the variance of the function value at the specified point.
+	It is assumed that there are no correlations amongst the entries in x.
+	'''
+
+	dim = len(x)
+
+	var = 0
+	for i in dim:
+		xNew = np.copy(x)
+
+		xNew[i] *= 1 + eps
+		f1 = func(xNew)
+		xNew[i] /= 1 + eps
+		xNew[i] *= 1 - eps
+		f2 = func(xNew)
+
+		df = (f1 - f2) / 2
+
+		var += (dx[i] * df)**2
+
+	return var
