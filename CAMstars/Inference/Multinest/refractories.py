ae187aed9686f42d6b73154f91d9c67ed05bd40f
Adam Jermyn <adamjermyn@gmail.com>
2018-02-09 12:34:16 +0000

    Refactored error propagation logic.

--- CAMstars/Inference/Multinest/refractories.py
+++ CAMstars/Inference/Multinest/refractories.py
@@ -12,12 +12,40 @@ to be fixed reference values.
 '''
 
 import numpy as np
-from multinestWrapper import run, analyze, plot1D, plot2D
+from CAMstars.Inference.Multinest.multinestWrapper import run, analyze, plot1D, plot2D
 from CAMstars.Parsers.accretingStars import accretingPop
 from CAMstars.Parsers.AJMartinStars import AJMartinPop
 from CAMstars.Parsers.LFossatiStars import LFossatiPop
+from CAMstars.AccretedFraction.star import star
+from CAMstars.Misc.constants import mSun, yr
+from CAMstars.Misc.utils import propagate_errors
 
 # Combine the field populations
 field = AJMartinPop + LFossatiPop
 
 # Calculate accreted fractions
+def fraction(mass, radius, temperature, u_rot, logmdot):
+
+	# u_rot is vsini in km/s
+	# logmdot is non-dimensionalised by Msun/yr
+	# Now we convert these to C.G.S.
+
+	mdot = 10**(logmdot) * (mSun / yr)
+	u_rot *= 1e5
+
+	s = star(mass, radius, temperature)
+
+	# For this we assume that the accreted material is lighter than the stellar material
+	return s.findF(u_rot, mdot, gradient=False) 
+
+x = [(p.params['M'], p.params['R'], p.params['T'], p.params['vrot'], p.params['logmdot']) for p in accretingPop]
+dx = [(p.params['dM'], p.params['dR'], p.params['dT'], p.params['dvrot'], p.params['dlogmdot']) for p in accretingPop]
+
+f = [fraction(*y) for y in x]
+df = [propagate_errors(fraction, y, dy) for y,dy in zip(*(x, dx))]
+
+f = np.array(f)
+df = np.array(df)
+
+print(f)
+print(df)
