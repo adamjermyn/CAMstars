9737bde71ffabbde9444e170c17273f87168a59e
Adam Jermyn <adamjermyn@gmail.com>
2018-02-15 11:54:03 +0000

    Merged parsers.

---
+++ CAMstars/Parsers/stars.py
@@ -0,0 +1,67 @@
+import os
+from glob import glob
+from CAMstars.Material.material import material
+from CAMstars.Material.population import population
+
+def parse(fname):
+	fi = open(fname,'r')
+	name = fname[fname.rfind('/')+1:fname.find('csv')-1]
+	names = []
+	logX = []
+	dlogX = []
+
+	# Load bulk stellar properties
+	params = {}
+	for i,line in enumerate(fi):
+		s = line.rstrip().split(' ')
+		if 'Element' in line:
+			break
+		s[0] = s[0][:-1]
+		try:
+			params[s[0]] = float(s[1])
+		except ValueError:
+			params[s[0]] = s[1]
+
+	# Calculate additional parameters
+	params['dlogmdot'] = 0.5 * (params['dlogmdotMinus'] + params['dlogmdotPlus'])
+
+	# Load abundance data
+	for i,line in enumerate(fi):
+		if 'References' in line:
+			break
+		s = line.rstrip().split(',')
+		if len(s) == 3:
+			names.append(s[0])	
+			logX.append(float(s[1]))
+			dlogX.append(float(s[2]))
+
+	logX = np.array(logX)
+	dlogX = np.array(dlogX)
+
+	# Correct for different normalizations
+	if params['Abundance Normalization'] == 'Ntot':
+		nH = 1 - sum(10**w for w in logX)
+		logX = logX - np.log10(nH)
+	elif params['Abundance Normalization'] == 'H12':
+		logX -= 12
+
+	# Correct for uncertainties
+	if 'General uncertainty' in params.keys():
+		dlogX = (dlogX**2 + params['General uncertainty']**2)**0.5
+
+	return material(name, names, logX, dlogX, params=params)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+files = glob(dir_path + '/../../Data/Accreting Stars/*.csv')
+materials = list([parse(f) for f in files])
+accretingPop = population(materials)
+
+files = glob(dir_path + '/../../Data/Field Stars/AJMartin/*.csv')
+materials = list([parse(f) for f in files])
+AJMartinPop = population(materials)
+
+files = glob(dir_path + '/../../Data/Field Stars/LFossati/*.csv')
+materials = list([parse(f) for f in files])
+LFossatiPop = population(materials)
+
+sol = parse(dir_path + '/../../Data/Field Stars/Sol.csv')
