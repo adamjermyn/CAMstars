9b10719454c98ca932dc75b5fa698eccad8ab1b9
Adam Jermyn <adamjermyn@gmail.com>
2019-03-05 12:30:33 -0800

    Changed scripts to rely on centralized filtering.

--- CAMstars/Inference/Multinest/RefractoryDepletion/fixedFx_subset_fAcc=1.py
+++ CAMstars/Inference/Multinest/RefractoryDepletion/fixedFx_subset_fAcc=1.py
@@ -17,116 +17,14 @@ and f_X = 0 for those with T_c < 200K.
 import numpy as np
 import os
 from CAMstars.Inference.Multinest.multinestWrapper import run, analyze, plot1D, plot2D
-from CAMstars.Parsers.stars import accretingPop, AJMartinPop, LFossatiPop, sol
 from CAMstars.Parsers.condensation import condenseTemps
 from CAMstars.AccretedFraction.star import star
 from CAMstars.Material.population import population
 from CAMstars.Misc.constants import mSun, yr
 from CAMstars.Misc.utils import propagate_errors, gaussianLogLike
 
-# Combine the field populations
-field = AJMartinPop + LFossatiPop
-
-# Exclude elements that have unreliable error estimates
-
-exclude_Temp = [
-'UCAC12284506',
-'UCAC12284746',
-'UCAC12065075',
-'UCAC12284653',
-'UCAC12284594',
-'UCAC12065058',
-'V380 Ori B'
-]
-
-for m in field.materials:
-	if m.name in exclude_Temp:
-		field.materials.remove(m)
-for m in accretingPop.materials:
-	if m.name in exclude_Temp:
-		accretingPop.materials.remove(m)
-
-exclude_S = [
-'HD100546', 
-'HD31648',
-'HD36112',
-'HD68695',
-'HD179218',
-'HD244604',
-'HD123269',
-'UCAC11105213',
-'UCAC11105379',
-'T Ori' 
-]
-
-for m in field.materials:
-	if m.name in exclude_S:
-		ind = m.queryIndex('S')
-		if ind is not None:			
-			m.names.pop(ind)
-			m.logX = np.delete(m.logX, ind)
-			m.dlogX = np.delete(m.dlogX, ind)
-
- 
-exclude_Zn = [
-'UCAC11105106',
-'UCAC11105213',
-'UCAC11105379'
-]
-
-for m in accretingPop.materials:
-	if m.name != 'HD144432':
-		if 'Zn' in m.names:
-			ind = m.queryIndex('Zn')
-			if ind is not None:
-				m.names.pop(ind)
-				m.logX = np.delete(m.logX, ind)
-				m.dlogX = np.delete(m.dlogX, ind)
-
-for m in field.materials:
-	if m.name in exclude_Zn:
-		ind = m.queryIndex('Zn')
-		if ind is not None:		
-			m.names.pop(ind)
-			m.logX = np.delete(m.logX, ind)
-			m.dlogX = np.delete(m.dlogX, ind)
-
-include_Na = [
-'HD139614',
-'HD144432'
-]
-
-for m in accretingPop.materials:
-	if m.name not in include_Na:
-		ind = m.queryIndex('Na')
-		if ind is not None:
-			m.names.pop(ind)
-			m.logX = np.delete(m.logX, ind)
-			m.dlogX = np.delete(m.dlogX, ind)
-
-# Filter out all but whitelisted elements
-whitelist = ['Fe','Mg','Si','Ti','H','He','C','O','Zn','Na','S']
-for m in accretingPop.materials:
-	toremove = set(m.names).difference(set(whitelist))
-	for n in toremove:
-		ind = m.queryIndex(n)
-		m.names.pop(ind)
-		m.logX = np.delete(m.logX, ind)
-		m.dlogX = np.delete(m.dlogX, ind)
-for m in field.materials:
-	toremove = set(m.names).difference(set(whitelist))
-	for n in toremove:
-		ind = m.queryIndex(n)
-		m.names.pop(ind)
-		m.logX = np.delete(m.logX, ind)
-		m.dlogX = np.delete(m.dlogX, ind)
-
-field = population(field.materials)
-accretingPop = population(accretingPop.materials)
-
-# Filter out stars with no known Mdot
-accretingPop = population([m for m in accretingPop.materials if 'logfAcc' in m.params.keys() and 'dlogfAcc' in m.params.keys()])
-stars = accretingPop.materials
+from CAMstars.Parsers.stars import sol
+from CAMstars.Parsers.whitelist_pop_filter import field, accretingPop, stars
 
 # Extract accreted fractions
 logf = np.zeros(len(stars))
--- CAMstars/Inference/Multinest/RefractoryDepletion/refractoriesFixedFX.py
+++ CAMstars/Inference/Multinest/RefractoryDepletion/refractoriesFixedFX.py
@@ -17,99 +17,16 @@ and f_X = 0 for those with T_c < 200K.
 import numpy as np
 import os
 from CAMstars.Inference.Multinest.multinestWrapper import run, analyze, plot1D, plot2D
-from CAMstars.Parsers.stars import accretingPop, AJMartinPop, LFossatiPop, sol
 from CAMstars.Parsers.condensation import condenseTemps
 from CAMstars.AccretedFraction.star import star
 from CAMstars.Material.population import population
 from CAMstars.Misc.constants import mSun, yr
 from CAMstars.Misc.utils import propagate_errors, gaussianLogLike
 
-# Combine the field populations
-field = AJMartinPop + LFossatiPop
+from CAMstars.Parsers.stars import sol
+from CAMstars.Parsers.pop_filter import field, accretingPop, stars
 
-# Exclude elements that have unreliable error estimates
 
-exclude_Temp = [
-'UCAC12284506',
-'UCAC12284746',
-'UCAC12065075',
-'UCAC12284653',
-'UCAC12284594',
-'UCAC12065058',
-'V380 Ori B'
-]
-
-for m in field.materials:
-	if m.name in exclude_Temp:
-		field.materials.remove(m)
-for m in accretingPop.materials:
-	if m.name in exclude_Temp:
-		accretingPop.materials.remove(m)
-
-exclude_S = [
-'HD100546', 
-'HD31648',
-'HD36112',
-'HD68695',
-'HD179218',
-'HD244604',
-'HD123269',
-'UCAC11105213',
-'UCAC11105379',
-'T Ori' 
-]
-
-for m in field.materials:
-	if m.name in exclude_S:
-		ind = m.queryIndex('S')
-		if ind is not None:			
-			m.names.pop(ind)
-			m.logX = np.delete(m.logX, ind)
-			m.dlogX = np.delete(m.dlogX, ind)
-
- 
-exclude_Zn = [
-'UCAC11105106',
-'UCAC11105213',
-'UCAC11105379'
-]
-
-for m in accretingPop.materials:
-	if m.name != 'HD144432':
-		if 'Zn' in m.names:
-			ind = m.queryIndex('Zn')
-			if ind is not None:
-				m.names.pop(ind)
-				m.logX = np.delete(m.logX, ind)
-				m.dlogX = np.delete(m.dlogX, ind)
-
-for m in field.materials:
-	if m.name in exclude_Zn:
-		ind = m.queryIndex('Zn')
-		if ind is not None:		
-			m.names.pop(ind)
-			m.logX = np.delete(m.logX, ind)
-			m.dlogX = np.delete(m.dlogX, ind)
-
-include_Na = [
-'HD139614',
-'HD144432'
-]
-
-for m in accretingPop.materials:
-	if m.name not in include_Na:
-		ind = m.queryIndex('Na')
-		if ind is not None:
-			m.names.pop(ind)
-			m.logX = np.delete(m.logX, ind)
-			m.dlogX = np.delete(m.dlogX, ind)
-
-field = population(field.materials)
-accretingPop = population(accretingPop.materials)
-
-# Filter out stars with no known Mdot
-accretingPop = population([m for m in accretingPop.materials if 'logfAcc' in m.params.keys() and 'dlogfAcc' in m.params.keys()])
-stars = accretingPop.materials
 
 # Extract accreted fractions
 logf = np.array(list(s.params['logfAcc'] for s in stars))
