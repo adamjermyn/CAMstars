297936a49ef34f37000ce786749da18d3aa2f733
Adam Jermyn <adamjermyn@gmail.com>
2019-03-05 11:54:01 -0800

    Added new inference scripts.

---
+++ CAMstars/Inference/Multinest/GasDustRatio/individual_Fe.py
@@ -0,0 +1,240 @@
+'''
+This script uses Nested Sampling to infer the refractory fraction of each element
+from data of a population of stars.
+
+The abundance of element X in star i is taken to be
+
+X_i = X_t ((1-f_i) + f ((1 - f_X) + f_X delta_{d,*})),
+
+where f_i are the photospheric fractions of the stars, f_X are the refractory fractions,
+and delta_{d,*} are the enhancement/depletion fractions for the star. X_t are taken
+to be fixed reference values.
+
+In this inference problem we hold f_X = 1 for all X with condensation temperatures above 1000K
+and f_X = 0 for those with T_c < 200K.
+'''
+
+import numpy as np
+import os
+from CAMstars.Inference.Multinest.multinestWrapper import run, analyze, plot1D, plot2D
+from CAMstars.Parsers.stars import accretingPop, AJMartinPop, LFossatiPop, sol
+from CAMstars.Parsers.condensation import condenseTemps
+from CAMstars.AccretedFraction.star import star
+from CAMstars.Material.population import population
+from CAMstars.Material.material import material
+from CAMstars.Misc.constants import mSun, yr
+from CAMstars.Misc.utils import propagate_errors, gaussianLogLike
+
+# Combine the field populations
+field = AJMartinPop + LFossatiPop
+
+# Exclude elements that have unreliable error estimates
+
+exclude_Temp = [
+'UCAC12284506',
+'UCAC12284746',
+'UCAC12065075',
+'UCAC12284653',
+'UCAC12284594',
+'UCAC12065058',
+'V380 Ori B'
+]
+
+for m in field.materials:
+	if m.name in exclude_Temp:
+		field.materials.remove(m)
+for m in accretingPop.materials:
+	if m.name in exclude_Temp:
+		accretingPop.materials.remove(m)
+
+exclude_S = [
+'HD100546', 
+'HD31648',
+'HD36112',
+'HD68695',
+'HD179218',
+'HD244604',
+'HD123269',
+'UCAC11105213',
+'UCAC11105379',
+'T Ori' 
+]
+
+for m in field.materials:
+	if m.name in exclude_S:
+		ind = m.queryIndex('S')
+		if ind is not None:			
+			m.names.pop(ind)
+			m.logX = np.delete(m.logX, ind)
+			m.dlogX = np.delete(m.dlogX, ind)
+
+for m in accretingPop.materials:
+	if m.name in exclude_S:
+		ind = m.queryIndex('S')
+		if ind is not None:
+			m.names.pop(ind)
+			m.logX = np.delete(m.logX, ind)
+			m.dlogX = np.delete(m.dlogX, ind)
+
+exclude_Zn = [
+'UCAC11105106',
+'UCAC11105213',
+'UCAC11105379'
+]
+
+for m in accretingPop.materials:
+	if m.name != 'HD144432':
+		if 'Zn' in m.names:
+			ind = m.queryIndex('Zn')
+			if ind is not None:
+				m.names.pop(ind)
+				m.logX = np.delete(m.logX, ind)
+				m.dlogX = np.delete(m.dlogX, ind)
+
+for m in field.materials:
+	if m.name in exclude_Zn:
+		ind = m.queryIndex('Zn')
+		if ind is not None:		
+			m.names.pop(ind)
+			m.logX = np.delete(m.logX, ind)
+			m.dlogX = np.delete(m.dlogX, ind)
+
+include_Na = [
+'HD139614',
+'HD144432'
+]
+
+for m in accretingPop.materials:
+	if m.name not in include_Na:
+		ind = m.queryIndex('Na')
+		if ind is not None:
+			m.names.pop(ind)
+			m.logX = np.delete(m.logX, ind)
+			m.dlogX = np.delete(m.dlogX, ind)
+
+# Whitelist of elements in accretingPop
+whiteList = ['Fe']
+for m in accretingPop.materials:
+	nn = list(m.names)
+	for e in nn:
+		if e not in whiteList:
+			ind = m.queryIndex(e)
+			m.names.pop(ind)
+			m.logX = np.delete(m.logX, ind)
+			m.dlogX = np.delete(m.dlogX, ind)
+
+
+field = population(field.materials)
+accretingPop = population(accretingPop.materials)
+
+# Filter out stars with no known Mdot
+accretingPop = population([m for m in accretingPop.materials if 'logfAcc' in m.params.keys() and 'dlogfAcc' in m.params.keys()])
+stars = accretingPop.materials
+
+def fElements(e):
+	# Returns the refractory fraction for the element
+
+	if condenseTemps[e] > 1000:
+		return 1
+	else:
+		return 0
+
+def infer(s):
+	# Performs an inference for one star.
+
+	reference = field
+
+	# Extract accreted fractions
+	logf = s.params['logfAcc']
+	dlogf = s.params['dlogfAcc']
+
+	# The formalism has trouble with fixing some parameters but not others, so we assign an error of 0.01 to any logf's that have zero error.
+	if dlogf == 0:
+		dlogf += 0.01
+
+	# Determine elements with enough information for inference
+	elements = list(e for e in s.names if e in reference.species)
+
+	# Sort elements by condensation temperature
+	elements = sorted(elements, key=lambda x: condenseTemps[x])
+
+	# Compute refractory fraction
+	fX = list(fElements(e) for e in elements)
+
+	# Compute difference and uncertainty
+	diff = list(s.query(e)[0] - reference.queryStats(e)[0] for e in elements)
+	var = list(s.query(e)[1]**2 + reference.queryStats(e)[1]**2 for e in elements)
+
+	print(s.name, s.query(e)[0], reference.queryStats(e)[0])
+
+	def probability(params):
+		logfAcc = params[0]
+		logd = params[1]
+
+		fAcc = 10**logfAcc
+		if fAcc > 1:
+			fAcc = 1
+
+		q = [np.log((1-fAcc) + fAcc * (1-fX[elements.index(e)] + 10**(logd)*fX[elements.index(e)])) for e in s.names if e in elements]
+
+		like = [gaussianLogLike((diff[j] - q[j])/var[j]**0.5) for j in range(len(q))]
+		like = sum(like)
+		like += gaussianLogLike((logfAcc - logf) / dlogf)
+
+		return like
+
+	dir_path = os.path.dirname(os.path.realpath(__file__))
+	oDir = dir_path + '/../../../../Output/GasDust_Fe_' + s.name + '/'
+	oDir = os.path.abspath(oDir)
+	oPref = 'Ref'
+	parameters = ['$\log f$','$\log \delta']
+	ranges = [(logf - 3 * dlogf, min(0, logf + 3*dlogf)), (-3,3)]
+	ndim = len(ranges)
+
+	for i,p in enumerate(parameters):
+		print(i, p)
+
+	run(oDir, oPref, ranges, parameters, probability)
+	a, meds = analyze(oDir, oPref, oDir, oPref)
+
+	# Plot abundances
+	import matplotlib.pyplot as plt
+	plt.style.use('ggplot')
+
+	logfAcc = meds[0]
+	logd = meds[1]
+
+	fAcc = 10**np.array(logfAcc)
+	if fAcc > 1:
+		fAcc = 1
+
+	refX = np.array([reference.queryStats(e)[0] for e in elements])
+	refV = np.array([reference.queryStats(e)[1] for e in elements])
+	model = np.array([reference.queryStats(e)[0] + np.log(1-fAcc + fAcc * (1-fX[elements.index(e)] + 10**(logd)*fX[elements.index(e)])) for e in elements])
+	outX = np.array([s.query(e)[0] for e in elements])
+	outV = np.array([s.query(e)[1] for e in elements])
+
+	fig = plt.figure()
+	ax = fig.add_subplot(211)
+	ax.errorbar(range(len(model)), refX, yerr=refV, c='c')
+	ax.scatter(range(len(model)), model,c='b')
+	ax.errorbar(range(len(model)),outX,yerr=outV, fmt='o',c='r')
+	ax.set(xticks=range(len(model)), xticklabels=elements)
+	ax.set_ylabel('$\log [X]$')
+	ax = fig.add_subplot(212)
+	ax.scatter(range(len(model)), outX - model,c='b')
+	ax.set(xticks=range(len(model)), xticklabels=elements)
+	ax.set_ylabel('Residuals')
+	plt.savefig(oDir + '/model.pdf')
+	plt.clf()
+
+	plot1D(a, parameters, oDir, oPref)
+	plot2D(a, parameters, oDir, oPref)
+
+	import pickle
+	pickle.dump(a, open(oDir+'/' + 'analyzer.pickle','wb'))
+
+for s in stars:
+	infer(s)
+
+
---
+++ CAMstars/Inference/Multinest/RefractoryDepletion/fixedFx_subset.py
@@ -0,0 +1,253 @@
+'''
+This script uses Nested Sampling to infer the refractory fraction of each element
+from data of a population of stars.
+
+The abundance of element X in star i is taken to be
+
+X_i = X_t ((1-f_i) + f ((1 - f_X) + f_X delta_{d,*})),
+
+where f_i are the photospheric fractions of the stars, f_X are the refractory fractions,
+and delta_{d,*} are the enhancement/depletion fractions for the star. X_t are taken
+to be fixed reference values.
+
+In this inference problem we hold f_X = 1 for all X with condensation temperatures above 1000K
+and f_X = 0 for those with T_c < 200K.
+'''
+
+import numpy as np
+import os
+from CAMstars.Inference.Multinest.multinestWrapper import run, analyze, plot1D, plot2D
+from CAMstars.Parsers.stars import accretingPop, AJMartinPop, LFossatiPop, sol
+from CAMstars.Parsers.condensation import condenseTemps
+from CAMstars.AccretedFraction.star import star
+from CAMstars.Material.population import population
+from CAMstars.Misc.constants import mSun, yr
+from CAMstars.Misc.utils import propagate_errors, gaussianLogLike
+
+# Combine the field populations
+field = AJMartinPop + LFossatiPop
+
+# Exclude elements that have unreliable error estimates
+
+exclude_Temp = [
+'UCAC12284506',
+'UCAC12284746',
+'UCAC12065075',
+'UCAC12284653',
+'UCAC12284594',
+'UCAC12065058',
+'V380 Ori B'
+]
+
+for m in field.materials:
+	if m.name in exclude_Temp:
+		field.materials.remove(m)
+for m in accretingPop.materials:
+	if m.name in exclude_Temp:
+		accretingPop.materials.remove(m)
+
+exclude_S = [
+'HD100546', 
+'HD31648',
+'HD36112',
+'HD68695',
+'HD179218',
+'HD244604',
+'HD123269',
+'UCAC11105213',
+'UCAC11105379',
+'T Ori' 
+]
+
+for m in field.materials:
+	if m.name in exclude_S:
+		ind = m.queryIndex('S')
+		if ind is not None:			
+			m.names.pop(ind)
+			m.logX = np.delete(m.logX, ind)
+			m.dlogX = np.delete(m.dlogX, ind)
+
+ 
+exclude_Zn = [
+'UCAC11105106',
+'UCAC11105213',
+'UCAC11105379'
+]
+
+for m in accretingPop.materials:
+	if m.name != 'HD144432':
+		if 'Zn' in m.names:
+			ind = m.queryIndex('Zn')
+			if ind is not None:
+				m.names.pop(ind)
+				m.logX = np.delete(m.logX, ind)
+				m.dlogX = np.delete(m.dlogX, ind)
+
+for m in field.materials:
+	if m.name in exclude_Zn:
+		ind = m.queryIndex('Zn')
+		if ind is not None:		
+			m.names.pop(ind)
+			m.logX = np.delete(m.logX, ind)
+			m.dlogX = np.delete(m.dlogX, ind)
+
+include_Na = [
+'HD139614',
+'HD144432'
+]
+
+for m in accretingPop.materials:
+	if m.name not in include_Na:
+		ind = m.queryIndex('Na')
+		if ind is not None:
+			m.names.pop(ind)
+			m.logX = np.delete(m.logX, ind)
+			m.dlogX = np.delete(m.dlogX, ind)
+
+# Filter out all but whitelisted elements
+whitelist = ['Fe','Mg','Si','Ti','H','He','C','O','Zn','Na','S']
+for m in accretingPop.materials:
+	toremove = set(m.names).difference(set(whitelist))
+	for n in toremove:
+		ind = m.queryIndex(n)
+		m.names.pop(ind)
+		m.logX = np.delete(m.logX, ind)
+		m.dlogX = np.delete(m.dlogX, ind)
+for m in field.materials:
+	toremove = set(m.names).difference(set(whitelist))
+	for n in toremove:
+		ind = m.queryIndex(n)
+		m.names.pop(ind)
+		m.logX = np.delete(m.logX, ind)
+		m.dlogX = np.delete(m.dlogX, ind)
+
+field = population(field.materials)
+accretingPop = population(accretingPop.materials)
+
+# Filter out stars with no known Mdot
+accretingPop = population([m for m in accretingPop.materials if 'logfAcc' in m.params.keys() and 'dlogfAcc' in m.params.keys()])
+stars = accretingPop.materials
+
+# Extract accreted fractions
+logf = np.array(list(s.params['logfAcc'] for s in stars))
+dlogf = np.array(list(s.params['dlogfAcc'] for s in stars))
+
+elements = list(e for e in accretingPop.species if e in field.species)
+
+# Sort elements by condensation temperature
+elements = sorted(elements, key=lambda x: condenseTemps[x])
+
+fixedList1 = ['Fe','Mg','Si','Ti']
+fixedList0 = ['H','He']
+freeList = ['C','O','Zn','Na','S']
+
+def indicator(x):
+	if x in fixedList1:
+		return 1
+	elif x in fixedList0:
+		return 0
+	else:
+		return None
+
+fixedElements = {e:indicator(e) for e in elements}
+freeElements = list(e for e in elements if fixedElements[e] is None)
+
+diff = list([star.logX[i] - field.queryStats(e)[0] for i,e in enumerate(star.names) if e in elements] for star in stars)
+var = list([field.queryStats(e)[1]**2 + star.dlogX[i]**2 for i,e in enumerate(star.names) if e in elements] for star in stars)
+
+# The formalism has trouble with fixing some parameters but not others, so we assign an error of 0.01 to any logf's that have zero error.
+dlogf[dlogf == 0] += 0.01
+
+def probability(params):
+	nS = len(stars)
+	logfAcc = params[:nS]
+	logd = params[nS:2*nS]
+	fX = params[2*nS:]
+
+	# Expand fX to include fixed elements
+	fX = np.array(list(fX[freeElements.index(e)] if e in freeElements else fixedElements[e] for e in elements))
+
+	fAcc = 10**logfAcc
+	fAcc[fAcc > 1] = 1
+
+	q = [[np.log((1-fAcc[i]) + fAcc[i] * (1-fX[elements.index(e)] + 10**(logd[i])*fX[elements.index(e)])) for e in m.names if e in elements] for i,m in enumerate(stars)]
+
+	like = [[gaussianLogLike((diff[i][j] - q[i][j])/var[i][j]**0.5) for j in range(len(q[i]))] for (i,m) in enumerate(stars)]
+	like = sum(sum(l) for l in like)
+	like += np.sum(gaussianLogLike((logfAcc - logf) / dlogf))
+
+	return like
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+oDir = dir_path + '/../../../../Output/FixedFX_Subset_Field/'
+oDir = os.path.abspath(oDir)
+oPref = 'Ref'
+parameters = [s.name + ' $\log f$' for s in stars] + [s.name + ' $\log \delta$' for s in stars] + ['$f_{\mathrm{' + e + '}}$' for e in freeElements]
+ranges = [(lf - 3 * dlf,min(0, lf + 3 * dlf)) for lf, dlf in zip(*(logf, dlogf))] + len(stars) * [(-3,3)] + len(freeElements) * [(0,1)]
+ndim = len(ranges)
+
+for i,p in enumerate(parameters):
+	print(i, p)
+
+print(elements)
+print(freeElements)
+
+run(oDir, oPref, ranges, parameters, probability)
+a, meds = analyze(oDir, oPref, oDir, oPref)
+
+# Plot abundances
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+nS = len(stars)
+logfAcc = meds[:nS]
+logd = meds[nS:2*nS]
+fX = meds[2*nS:]
+# Expand fX to include fixed elements
+fX = np.array(list(fX[freeElements.index(e)] if e in freeElements else fixedElements[e] for e in elements))
+
+fAcc = 10**np.array(logfAcc)
+fAcc[fAcc > 1] = 1
+
+solX = [[sol.query(e)[0] for e in m.names if e in elements] for i,m in enumerate(stars)]
+solV = [[sol.query(e)[1] for e in m.names if e in elements] for i,m in enumerate(stars)]
+refs = [[field.queryStats(e)[0] for e in m.names if e in elements] for i,m in enumerate(stars)]
+refsv = [[field.queryStats(e)[1] for e in m.names if e in elements] for i,m in enumerate(stars)]
+model = [[field.queryStats(e)[0] + np.log((1-fAcc[i]) + fAcc[i] * (1-fX[elements.index(e)] + 10**(logd[i])*fX[elements.index(e)])) for e in m.names if e in elements] for i,m in enumerate(stars)]
+outs = [[m.query(e)[0] for e in m.names if e in elements] for m in stars]
+outsv = [[m.query(e)[1] for e in m.names if e in elements] for m in stars]
+outsn = [[e for e in m.names if e in elements] for m in stars]
+
+solX = list(np.array(s) for s in solX)
+solV = list(np.array(s) for s in solV)
+refs = list(np.array(r) for r in refs)
+refsv = list(np.array(r) for r in refsv)
+model = list(np.array(m) for m in model)
+outs = list(np.array(o) for o in outs)
+outsv = list(np.array(o) for o in outsv)
+outsn = list(np.array(o) for o in outsn)
+
+for i,star in enumerate(stars):
+	fig = plt.figure()
+	ax = fig.add_subplot(211)
+	ax.errorbar(range(len(model[i])), solX[i], yerr=solV[i], c='c')
+	ax.errorbar(range(len(model[i])), refs[i], yerr=refsv[i], c='k')
+	ax.scatter(range(len(model[i])), model[i],c='b')
+	ax.errorbar(range(len(model[i])),outs[i],yerr=outsv[i], fmt='o',c='r')
+	ax.set(xticks=range(len(model[i])), xticklabels=outsn[i])
+	ax.set_ylabel('$\log [X]$')
+	ax = fig.add_subplot(212)
+	ax.scatter(range(len(model[i])), outs[i] - model[i],c='b')
+	ax.set(xticks=range(len(model[i])), xticklabels=outsn[i])
+	ax.set_ylabel('Residuals')
+	plt.savefig(oDir + '/' + star.name + '_model.pdf')
+	plt.clf()
+
+
+plot1D(a, parameters, oDir, oPref)
+plot2D(a, parameters, oDir, oPref)
+
+import pickle
+pickle.dump(a, open(oDir+'/' + 'analyzer.pickle','wb'))
+
+
