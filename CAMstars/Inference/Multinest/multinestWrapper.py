2196dad9cde330230daf9781f96f4908de161d58
Adam Jermyn <adamjermyn@gmail.com>
2022-06-10 10:05:53 -0400

    Updates

--- CAMstars/Inference/Multinest/multinestWrapper.py
+++ CAMstars/Inference/Multinest/multinestWrapper.py
@@ -5,7 +5,6 @@ import pymultinest
 from pymultinest.solve import solve
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
-from mpi4py import MPI
 
 def Prior(cube, ranges):
 	'''
@@ -37,15 +36,16 @@ def run(outputDirectory, outputPrefix, ranges, parameters, loglikelihood, n_live
 
 	prefix = outputDirectory + '/' + outputPrefix
 
-	if MPI.COMM_WORLD.Get_rank() == 0:
-		if not os.path.exists(outputDirectory):
-			os.makedirs(outputDirectory)
+	if not os.path.exists(outputDirectory):
+		os.makedirs(outputDirectory)
 
 	ndim = len(ranges)
 
 	def pri(cube):
 		return Prior(cube, ranges)
 
+
+	print(prefix)
 	result = solve(LogLikelihood=loglikelihood, Prior=pri, n_dims = ndim, importance_nested_sampling=False,resume=False,outputfiles_basename=prefix,verbose=True, n_live_points=n_live_points)
 
 	print()
@@ -56,17 +56,16 @@ def run(outputDirectory, outputPrefix, ranges, parameters, loglikelihood, n_live
 	for name, col in zip(parameters, result['samples'].transpose()):
 		print('%15s : %.3f +- %.3f' % (name, col.mean(), col.std()))
 
-	if MPI.COMM_WORLD.Get_rank() == 0:
-		with open('%sparams.json' % prefix, 'w') as f:
-			json.dump(parameters, f, indent=2)
+	with open('%sparams.json' % prefix, 'w') as f:
+		json.dump(parameters, f, indent=2)
 
-		parameters = json.load(open(prefix + 'params.json'))
-		n_params = len(parameters)
+	parameters = json.load(open(prefix + 'params.json'))
+	n_params = len(parameters)
 
-		a = pymultinest.Analyzer(n_params = n_params, outputfiles_basename = prefix)
-		s = a.get_stats()
+	a = pymultinest.Analyzer(n_params = n_params, outputfiles_basename = prefix)
+	s = a.get_stats()
 
-		json.dump(s, open(prefix + 'stats.json', 'w'), indent=4)
+	json.dump(s, open(prefix + 'stats.json', 'w'), indent=4)
 
 def analyze(inputDirectory, inputPrefix, outputDirectory, outputPrefix):
 	'''
